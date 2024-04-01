b0d5cb9500ac16d16d3169eba496fde100efcbc3
Krum Bezmenov <krum.bezmenov@gmail.com>
2024-04-01 22:35:04 +0300

    Add app and home components

---
+++ books-app/src/app/api.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Theme, themeDetails } from './types/theme';
+import { Post } from './types/post';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+const apiUrl = environment.themesUrl;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  //
+  // private theme$$ = new BehaviorSubject<themeDetails | undefined>(undefined);
+  constructor(private httpClient: HttpClient) {}
+
+  getThemes() {
+    return this.httpClient.get<Theme[]>(`${apiUrl}`);
+  }
+
+  getTheme(id: string) {
+    return this.httpClient.get<Theme>(`${apiUrl}/${id}/details`);
+  }
+
+  // createTheme(
+  //   title: string,
+  //   genre: string,
+  //   author: string,
+  //   image: string,
+  //   description: string
+  // ) {
+  //   const payload = { title, genre, author, image, description };
+  //   return this.httpClient.post<Theme>(`${apiUrl}`, payload);
+  // }
+
+  createTheme(themeData: Theme, token: string): Observable<any> {
+    const headerObject = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    };
+
+    return this.httpClient.post<Theme>(`${apiUrl}/create`, themeData, {
+      headers: headerObject,
+    });
+  }
+
+  getLatestThemes(limit?: number) {
+    const { apiUrl } = environment;
+    let url = `${apiUrl}/themes`;
+
+    if (limit) {
+      url += `?limit=${limit}`;
+    }
+
+    return this.httpClient.get<Theme[]>(url);
+  }
+
+  deleteThemeById(id: string, token: string): Observable<any> {
+    const headerObject = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    };
+
+    return this.httpClient.get(`${apiUrl}/${id}/delete`, {
+      headers: headerObject,
+    });
+  }
+
+  saveThemeById(id: string, themeData: Theme, token: string): Observable<any> {
+    const headerObject = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    };
+
+    return this.httpClient.post(`${apiUrl}/${id}/update`, themeData, {
+      headers: headerObject,
+    });
+  }
+
+  // updateTheme(
+  //   title: string,
+  //   genre: string,
+  //   author: string,
+  //   image: string,
+  //   description: string
+  // ) {
+  //   return this.httpClient
+  //     .post<Theme>(`auth/themes/update`, {
+  //       title,
+  //       genre,
+  //       author,
+  //       image,
+  //       description,
+  //     })
+  //     .pipe(tap((theme) => this.theme$$.next(theme)));
+  // }
+}
---
+++ books-app/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ThemesListComponent } from './theme/themes-list/themes-list.component';
+import { RecentPostsComponent } from './theme/recent-posts/recent-posts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './theme/main/main.component';
+import { SharedModule } from './shared/shared.module';
+import { HomeStaticComponent } from './home-static/home-static.component';
+import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
+import { ThemeModule } from './theme/theme.module';
+import { ErrorComponent } from './error/error.component';
+import { FormsModule } from '@angular/forms';
+import { appInterceptorProvider } from './app.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeStaticComponent,
+    HomeComponent,
+    ErrorComponent,
+  ],
+  imports: [
+    BrowserModule,
+
+    CoreModule,
+    SharedModule,
+    HttpClientModule,
+    UserModule,
+    ThemeModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [appInterceptorProvider],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
